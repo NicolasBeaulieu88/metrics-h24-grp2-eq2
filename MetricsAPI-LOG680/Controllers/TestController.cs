efb20dc67db8b6df504e92235060ada1b64d10da
Santiago Chiappe <47071792+SantiagoChiappe@users.noreply.github.com>
2024-01-29 17:39:07 -0500

    Get Project WIP

--- MetricsAPI-LOG680/Controllers/TestController.cs
+++ MetricsAPI-LOG680/Controllers/TestController.cs
@@ -1,5 +1,8 @@
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
 using MetricsAPI_LOG680.DTO;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using Octokit;
 
 namespace MetricsAPI_LOG680.Controllers
@@ -29,7 +32,7 @@ namespace MetricsAPI_LOG680.Controllers
             // Replace these values with your GitHub username, repository name, and personal access token
             username ??= "NicolasBeaulieu88";
             repository ??= "metrics-h24-grp2-eq2";
-            token ??= "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";
+            token ??= "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";
 
             var client = new GitHubClient(new ProductHeaderValue("MyApp"));
 
@@ -43,7 +46,6 @@ namespace MetricsAPI_LOG680.Controllers
             {
                 // Get information about the repository
                 var repositoryInfo = await client.Repository.Get(username, repository);
-
                 msg = $"Repository: {repositoryInfo.Name}";
                 msg += $"Description: {repositoryInfo.Description}";
                 msg += $"Default Branch: {repositoryInfo.DefaultBranch}";
@@ -61,6 +63,81 @@ namespace MetricsAPI_LOG680.Controllers
             }
             
             return await Task.FromResult<ActionResult>(Ok(msg));
+        }
+
+        [HttpGet("TestGraphQL")]
+        public async Task<ActionResult> GetTestGraphQL(string? owner, string? repository, string? token)
+        {
+            // Replace these values with your GitHub username, repository name, and personal access token
+            owner ??= "NicolasBeaulieu88";
+            repository ??= "metrics-h24-grp2-eq2";
+            token ??= "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";
+
+            var address = "https://api.github.com/graphql";
+
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(address),
+                DefaultRequestHeaders =
+                {
+                    UserAgent =
+                    {
+                        new System.Net.Http.Headers.ProductInfoHeaderValue("YourApp", "1.0")
+                    },
+                    Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token)
+                }
+            };
+
+            var options = new GraphQLHttpClientOptions
+            {
+                EndPoint = new Uri(address)
+            };
+
+            var graphQLClient = new GraphQLHttpClient(options, new NewtonsoftJsonSerializer(), httpClient);
+
+            var graphQLRequest = new GraphQLHttpRequest
+            {
+                Query = @"
+                    query{
+                        repository(name: """ + repository + @""", owner: """ + owner + @"""){
+                            id
+                            name
+                            projectsV2(first: 20) {
+                                nodes {
+                                    id
+                                    title
+                                }
+                            }
+                        }
+                    }"
+            };
+
+            try
+            {
+                var graphQLResponse = await graphQLClient.SendQueryAsync<JObject>(graphQLRequest);
+                
+                var repositoryNode = graphQLResponse.Data["repository"];
+                var repositoryName = repositoryNode["name"].Value<string>();
+
+                try
+                {
+                    var projectsNode = repositoryNode["projectsV2"]["node"];
+                    var projectId = projectsNode[0]["id"].Value<int>();
+                    var projectName = projectsNode[0]["title"].Value<string>();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                Console.WriteLine($"Repository: {repositoryName}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return Ok();
         }
 
         [HttpPost(Name = "PostTest")]
