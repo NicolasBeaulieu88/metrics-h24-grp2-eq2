64669b4a128c3cb52a391a67d9e384fcbc1e1c59
Humberto Villarino <humbertovillarino@Humbertos-MacBook-Pro.local>
2024-02-04 20:41:01 -0500

    MIgrations

---
+++ MetricsAPI-LOG680/Migrations/20240205010902_LeadTimePerIssue2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MetricsAPI_LOG680.Migrations
+{
+    /// <inheritdoc />
+    public partial class LeadTimePerIssue2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_done",
+                table: "lead_time_per_issue",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_done",
+                table: "lead_time_per_issue");
+        }
+    }
+}
