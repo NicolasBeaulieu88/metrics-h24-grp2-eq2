eef8a2b07929e09edd9de632adf687e8e02debe0
Humberto Villarino <humbertovillarino@Humbertos-MBP.hitronhub.home>
2024-03-10 15:45:25 -0400

    build size

--- buildtools/buildContainer.sh
+++ buildtools/buildContainer.sh
@@ -14,8 +14,11 @@ end_time=$(date +"%s")
 # Calculate duration
 duration=$((end_time - start_time))
 
+# Get the size of the built image
+image_size=$(docker image inspect --format='{{.Size}}' metricsapi)
+
 # Insert build information into the PostgreSQL database
 PGPASSWORD="Dw2OtjzSOKoZvrGN" psql -h 157.230.69.113 -U user02eq2 -d db02eq2 <<EOF
-INSERT INTO build_logs (start_time, end_time, duration)
-VALUES (to_timestamp($start_time), to_timestamp($end_time), $duration);
+INSERT INTO build_logs (start_time, end_time, duration, image_size)
+VALUES (to_timestamp($start_time), to_timestamp($end_time), $duration, $image_size);
 EOF
