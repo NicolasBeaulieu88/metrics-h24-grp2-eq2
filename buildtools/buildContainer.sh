c319dce78b8aee4a7730f49ca9de3b9c4d8cdbc4
Humberto Villarino <humbertovillarino@Humbertos-MBP.hitronhub.home>
2024-03-10 12:07:11 -0400

    added first metric

---
+++ buildtools/buildContainer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start time of the build
+start_time=$(date +"%s")
+
+# Run Docker build
+cd ../MetricsAPI-LOG680 || exit
+docker build -t your_image_name .
+cd ../buildtools || exit
+
+# End time of the build
+end_time=$(date +"%s")
+
+# Calculate duration
+duration=$((end_time - start_time))
+
+# Insert build information into the PostgreSQL database
+PGPASSWORD="Dw2OtjzSOKoZvrGN" psql -h 157.230.69.113 -U user02eq2 -d db02eq2 <<EOF
+INSERT INTO build_logs (start_time, end_time, duration)
+VALUES (to_timestamp($start_time), to_timestamp($end_time), $duration);
+EOF
